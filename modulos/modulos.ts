4894e65ec392a0251b919e40304a9654b70b1b88
Davi Vitorino da Silva <davi.silva@softplan.com.br>
2023-07-07 19:59:22 -0300

    feat: modulos and namespaces lessons added

---
+++ modulos/modulos.ts
@@ -0,0 +1,8 @@
+import retangulo from "./retangulo";
+import { areaCircunferencia as circ } from "./circunferencia";
+
+console.log(retangulo(7, 8));
+console.log(circ(2));
+
+const { digaOi } = require('./novo');
+console.log(digaOi('Ana'))
