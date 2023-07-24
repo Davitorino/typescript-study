047743411fff8117c52129b6f8cd48471fd12ce8
Davi Vitorino da Silva <davi.silva@softplan.com.br>
2023-07-24 13:37:31 -0300

    feat: add lessons from react-ts submodule

---
+++ subprojects/react-ts/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Contador from './components/Contador';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Contador valorInicial={9876} />
+      <Contador />
+    </div>
+  );
+}
+
+export default App;
