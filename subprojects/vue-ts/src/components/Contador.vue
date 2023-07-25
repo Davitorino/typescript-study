b72f1d5055dbb297c004a718f03de7214bc4937c
Davi Vitorino da Silva <davi.silva@softplan.com.br>
2023-07-24 23:00:15 -0300

    feat: added lessons from vue-ts module

---
+++ subprojects/vue-ts/src/components/Contador.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>Contador</h1>
+    <h2>{{ valor }}</h2>
+    <button @click="setValor(-1)">
+      Decrementar
+    </button>
+    <button @click="setValor(1)">
+      Incrementar
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  export default Vue.extend({
+    name: 'AppContador',
+    props: {
+      valorInicial: {
+        type: Number,
+        default: 0,
+      }
+    },
+    data: () => ({
+      valor: 0,
+    }),
+    mounted() {
+        this.valor = this.valorInicial;
+    },
+    methods: {
+      setValor(delta: number): void {
+        this.valor += delta;
+      }
+    },
+  })
+</script>
+
+<style>
+
+</style>
---
+++ subprojects/vue-ts/src/components/ContadorClasse.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>Contador Classe</h1>
+    <h2>{{ valor }}</h2>
+    <h3>{{ getParOuImpar() }}</h3>
+    <h3>{{ parOuImpar }}</h3>
+    <button @click="setValor(-1)">
+      Decrementar
+    </button>
+    <button @click="setValor(1)">
+      Incrementar
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Vue, Component, Prop } from 'vue-property-decorator';
+
+  @Component
+  export default class ContadorClasse extends Vue {
+    @Prop(Number) private readonly valorInicial?: number;
+    public valor = this.valorInicial || 0;
+
+    public setValor(delta: number): void {
+      this.valor += delta;
+    }
+
+    public getParOuImpar(): string {
+      return this.valor % 2 === 0 ? 'Par' : 'Ímpar';
+    }
+
+    public get parOuImpar(): string {
+      return this.valor % 2 === 0 ? 'Par' : 'Ímpar';
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
